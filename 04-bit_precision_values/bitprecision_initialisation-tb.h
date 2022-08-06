e417f03ac93b4b8fcaca4a02a5f005df389f6b13
Sajjad Salem <31271355+salemsajjad@users.noreply.github.com>
2022-08-06 12:33:51 +0430

    Add files via upload

---
+++ 04-bit_precision_values/bitprecision_initialisation-tb.h
@@ -0,0 +1,16 @@
+#ifndef __ASSIGNMENT_TB_H__
+#define __ASSIGNMENT_TB_H__
+#include "bitprecision_initialisation.h"
+
+
+
+void bitprecision_initialisation(
+		ap_int<41> &a1,
+		ap_int<41> &a2,
+		ap_int<41> &a3,
+		ap_int<41> &a4,
+
+		ap_int<763> &b1,
+		ap_int<763> &b2,
+		ap_int<763> &b3);
+#endif //__ASSIGNMENT_TB_H__
